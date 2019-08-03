7ba6d78b9b7007d8f9a3a0c467deae3915107aa3
GonzaloPereira <gonzaloapr45@gmail.com>
2019-08-02 19:25:50 -0500

    edit

--- Codeforces/1199C.cpp
+++ Codeforces/1199C.cpp
@@ -1,40 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n,I;
-int a[400005];
+#define N 400005
+int n,I,k,K;
+int a[N];
+vector<int> freq;
 int main(){
 	cin >> n >> I;
-	for(int i = 0; i < n ;i++){
-		cin >> a[i];
-	}
+	for(int i =0 ; i < n ;i++) scanf("%d",&a[i]);
+	k = (8*I)/n;
+	K = 1<<min(20,k);
+	int cnt = 0;
 	sort(a,a+n);
-	int K = 1;
-	for(int i = 1 ; i < n ; i++){
-		if(a[i]!=a[i-1]) K++;
+	for(int i = 0 ; i < n ; i++){
+		cnt++;
+		if(a[i]!=a[i+1]){ freq.push_back(cnt); cnt = 0; }
 	}
-	int k = (int)log2(K)+((int)log2(K)!=log2(K)?1:0), c = 0,l = 0, r = n-1,c1,c2,ll=0,rr=n-1;
-	while((n*k)>(8*I)){
-		c1 = c2 = 0;
-		for(int i = l ; i < r ; i++){
-			c1++; ll++;
-			if(a[i]!=a[i+1]) break;
-		}
-		for(int i = r ; i > l; i--){
-			c2++; rr--;
-			if(a[i]!=a[i-1]) break;
-		}
-		if(c1<c2){
-			l = ll;
-			rr = r;
-			c+=c1;
-		}else{
-			r =rr;
-			ll = l;
-			c+=c2;
-		}
-		K--;
-		k = (int)log2(K)+((int)log2(K)!=log2(K)?1:0);
+	int r = 0,nn = freq.size(),c = 0,maxi = INT_MIN;
+	for(int l = 0 ; l < nn ; l++){
+		while(r-l<K && r < nn) c+= freq[r], r++;
+		maxi = max(maxi,c);
+		c-=freq[l];
 	}
-	cout << c << endl;
+	cout << n - maxi << endl;
 	return 0;
 }
