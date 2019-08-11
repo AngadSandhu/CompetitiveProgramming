1ba6110032b8a09883ff05c0ebcbc82d15d7cc35
GonzaloPereira <gonzaloapr45@gmail.com>
2019-08-11 15:10:42 -0500

    edit

---
+++ 1200A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ri(x) scanf("%d",&(x))
+#define rll(x) scanf("%lld",&(x))
+#define N 0
+typedef long long ll;
+using namespace std;
+ 
+string s;
+int n;
+int a[100005];
+
+int main(){
+	
+	return 0;	
+}
+
